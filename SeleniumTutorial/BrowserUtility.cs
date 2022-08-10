35700e08dd2b83514e2d57520fb1b2714d3a2678
ulissera <ulissesbgd@gmail.com>
2022-08-09 21:20:25 -0300

    update2

---
+++ SeleniumTutorial/BrowserUtility.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTutorial.Utilities
+{
+    public class BrowserUtility
+    {
+        public IWebDriver Init(IWebDriver driver)
+        {
+            //Open the browser from this class
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+            return driver;
+        }
+    }
+}
