aa75cd35ab4717c84a03f409d4acd9a190ecc88f
ulissera <ulissesbgd@gmail.com>
2022-08-09 21:08:13 -0300

    update1

---
+++ SeleniumTutorial/OrderSkipAttribute.cs
@@ -0,0 +1,34 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTutorial.BaseClass;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTutorial
+{
+    [TestFixture]
+    public class OrderSkipAttribute
+    {
+        [Test, Category("OrderSkipAttribute")]
+        public void TestMethod1()
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com.br";
+            IWebElement email = driver.FindElement(By.XPath(".//*[@id = 'email']"));
+            email.SendKeys("USER");
+            driver.Close();
+
+        }
+
+    }
+}
